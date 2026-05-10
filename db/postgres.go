994018d1fd79ef029bb329a629c865f3ede540e7
Aditya Punmiya <adityapunmiya@gmail.com>
2026-05-10 22:25:10 +0530

    mapped custom pg datatype in go

--- db/postgres.go
+++ db/postgres.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -17,7 +18,21 @@ func connectPostgres(url string) (*PostgresParams, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	pool, err := pgxpool.New(ctx, url)
+	config, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse database url: %w", err)
+	}
+
+	config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		dt, err := conn.LoadType(ctx, "portfolio.position_create")
+		if err != nil {
+			return err
+		}
+		conn.TypeMap().RegisterType(dt)
+		return nil
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create connection pool: %w", err)
 	}
---
+++ lib/objects.go
@@ -0,0 +1,10 @@
+package lib
+
+type PositionCreate struct {
+	ID       int32   `db:"id"`
+	Role     string  `db:"role"`
+	Start    string  `db:"start"`
+	End      *string `db:"end"`
+	WorkDone string  `db:"workdone"`
+	Projects []int32 `db:"projects"`
+}
